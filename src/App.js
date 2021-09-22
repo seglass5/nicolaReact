fa5f184a0124246ef2b8ed03fa386f3e6eb99bd0
Glass <Sam.Glass@d-fine.com>
2021-09-22 17:21:30 +0100

    Some more jzhzhing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
-//import logo from './logo.svg';
-//import './App.css';
 import Header from "./components/Header";
+import Button from "./components/Button";
+import React, { useState } from 'react';
+//import InstagramEmbed from 'react-instagram-embed'
+
+import ns from './NS.jpg'
 
 function App() {
+  const [view, setView] = useState(false)
+  
+  const onClick = () => {
+    setView(!view)
+  }
+  
   return (
     <div className="container">
-      <Header title = "Nicola Kate Stebbing"/>
+      <Header title = "Nicola Kate Stebbing" />
       <p>
-        Nicola is Content Manager at Gitti.
+        Nicola is Content Manager at <a href={"https://www.gitticonsciousbeauty.de/en"}>Gitti</a>. 
       </p>
+      <p>
+        She makes <a href={"https://www.instagram.com/gittibeauty/"}>great content</a>. <a href={"https://www.instagram.com/pics_by_nics/"}>Seriously</a>.
+      </p>
+      <p>
+        She has a pet lemon called Lenny.
+      </p>  
+      <p>
+        I heard her boyfriend is <a href={"https://www.instagram.com/syzygie.music/"}>pretty cool</a>.
+      </p>
+      <Button onClick={onClick} color='steelblue' text='See More' />
+      {view ? <img src={ns} alt='Nicolasmells' style={{width:1000, height:900}} /> : ""}
+      {/* <InstagramEmbed
+        clientAccessToken='<appId>|<clientToken>'
+        url='https://instagr.am/p/Ytlfl/'
+        maxWidth={375}
+        hideCaption={false}
+        containerTagName='div'
+        injectScript
+        protocol=''
+        onLoading={() => {}}
+        onSuccess={() => {}}
+        onAfterRender={() => {}}
+        onFailure={() => {}}
+      /> */}
     </div>
   );
 }
