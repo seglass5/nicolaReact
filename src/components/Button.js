76443afdd4913eec52700c5a4e7817054821d21f
Glass <Sam.Glass@d-fine.com>
2021-09-22 18:04:35 +0100

    Adding untracked files to the commit

---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+const Button = ({ color, text, onClick }) => {
+    return (
+        <div>
+            <button style={{ backgroundColor: color }} className='btn' onClick = {onClick}>{text}</button>
+        </div>
+    )
+}
+
+export default Button
